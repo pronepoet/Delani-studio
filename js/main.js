a157189f2c1526cb2e64a1801f08aa58f3a00e58
pronepoet <ochiengphillip84@gmail.com>
2022-03-15 04:36:18 +0300

    Ft: add the hover effect for the portfolio items

--- js/main.js
+++ js/main.js
@@ -15,6 +15,71 @@ $(document).ready(function(){
         $(".dev_text").hide();
         $(".devImg").show();
     })
+    $(".manageImg").click(function(){
+        $(".manageImg").hide();
+        $(".manage_text").show();
+
+    })
+    $(".manage_text").click(function(){
+        $(".manage_text").hide();
+        $(".manageImg").show();
+    })
+    $(".portfolio-item1").mouseover(function(){
+        $(".overlay1").show();
+       
+    }) 
+    $(".portfolio-item1").mouseout(function(){
+        $(".overlay1").hide();
+       
+    }) 
+    $(".portfolio-item2").mouseover(function(){
+        $(".overlay2").show();
+       
+    }) 
+    $(".portfolio-item2").mouseout(function(){
+        $(".overlay2").hide();
+       
+    }) 
+    $(".portfolio-item3").mouseover(function(){
+        $(".overlay3").show();
+       
+    }) 
+    $(".portfolio-item3").mouseout(function(){
+        $(".overlay3").hide();
+       
+    }) 
+    $(".portfolio-item4").mouseover(function(){
+        $(".overlay4").show();
+       
+    }) 
+    $(".portfolio-item4").mouseout(function(){
+        $(".overlay4").hide();
+       
+    }) 
+    $(".portfolio-item5").mouseover(function(){
+        $(".overlay5").show();
+       
+    }) 
+    $(".portfolio-item5").mouseout(function(){
+        $(".overlay5").hide();
+       
+    }) 
+    $(".portfolio-item6").mouseover(function(){
+        $(".overlay6").show();
+       
+    }) 
+    $(".portfolio-item6").mouseout(function(){
+        $(".overlay6").hide();
+       
+    }) 
+    $(".portfolio-item8").mouseover(function(){
+        $(".overlay8").show();
+       
+    }) 
+    $(".portfolio-item8").mouseout(function(){
+        $(".overlay8").hide();
+       
+    }) 
 })
 
 
